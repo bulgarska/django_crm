2389fb09203c82de7a9d2380643eca877b7d8cd2
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2025-02-18 17:13:42 -0800

    Created base html file and integrated bootstrap5, css, and js. Created basic home page extending the base.

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, DetailView, CreateView, 
+                                  UpdateView, DeleteView, TemplateView)
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'crm/home.html'
