cb35ec71c07cb43393fac1f67901abb33f940b54
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2025-02-19 13:56:36 -0800

    Added register form and page.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views.generic import (ListView, DetailView, CreateView, 
                                   UpdateView, DeleteView, TemplateView)
+from .forms import UserRegisterForm
 
-class RegisterView(TemplateView):
+
+class RegisterView(CreateView):
     template_name = 'users/register.html'
+    form_class = UserRegisterForm()
+    success_url = reverse_lazy('login')
+    
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form_class
+        #form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            # saves user to database
+            form.save()
+            username = form.cleaned_data.get('username')
+            # Flash message that appears for account creation confirmation and disappears
+            messages.success(request, f'Welcome {username}, your account has been created! You are now able to login.')
+            return redirect('login')
+        
+        return render(request, self.template_name, {'form': form})
