8f038b1e77e95bfffa4cc0490c264c8f9875e5d4
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2025-02-19 22:49:59 -0800

    Records are displayed in a table on the home page.

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import (HomeView)
+from .views import (HomeView, RecordDetailView)
 
 urlpatterns = [
 	path('', HomeView.as_view(), name='crm-home'),
+ 	path('record/<int:pk>', RecordDetailView.as_view(), name='record-detail'),
+ 
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import (ListView, DetailView, CreateView, 
                                   UpdateView, DeleteView, TemplateView)
+from .models import Record
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'crm/home.html'
+    model = Record
+    context_object_name = 'records'
+    
+class RecordDetailView(DetailView):
+    model = Record
